ad6d688b151707744223aad9bac3a67f695d7374
YanagiEiichi <admin@web-tinker.com>
2015-07-28 16:39:09 +0800

    Add "watch" tool

--- README.md
+++ README.md
@@ -3,20 +3,20 @@
 这是一个 Web 前端工程化的工具包。
 
 
-### 安装
+### 安装方式
 
-```
+```bash
 npm install webspoon
 ```
 
 或者也可以全局安装
 
-```
+```bash
 sudo npm install webspoon -g
 ```
 
 
-### 工具
+### 提供的工具
 
 ##### webspoon-usemin
 
@@ -60,3 +60,23 @@ static 列表指定的是静态文件，它会根据其自身内容重命名到�
   2. base 列表中的文件更新后会覆盖原始文件，如果不希望被覆盖请先复制出来。
   3. static 列表中的文件操作是被重命名而不是复制，如需备份请提前。
 
+
+##### webspoon-watch
+
+用于给静态资的文件名中加入 hash，同时替换引用文件的引用路径。
+
+```bash
+webspoon-watch -target <FileList> -exec <Command>
+```
+
+传入两个参数 -target 和 -exec。
+
+target 参数用于指定需要监视的文件。
+
+exec 参数用于指定监视到文件变化后需要执行的脚本。
+
+在执行的脚本中可以通过 $src 变量来取到当前处理的文件路径（相对路径）。
+
+注：
+  1. 只有当文件内容有变化时才会触发。
+  2. 通配符初始解析，这意味着开始 watch 之后才创建的文件不会被 watch 到。
---
+++ src/watch.js
@@ -0,0 +1,77 @@
+#!/usr/bin/env node
+
+import bfs from 'babel-fs';
+import glob from 'glob';
+import crypto from 'crypto';
+import childProcess from 'child_process';
+
+
+/**
+ * 声明
+**/
+
+var getHash = file => bfs.readFile(file).then(data => {
+  let sha1 = crypto.createHash('sha1');
+  sha1.update(data);
+  return sha1.digest('hex');
+});
+
+
+/**
+ * 收集参数
+**/
+
+var watchingList = [];
+var commandList = [];
+process.argv.slice(2).reduce((base, item) => {
+  switch(item) {
+    case '-target': return watchingList;
+    case '-exec': return commandList;
+    default:
+      if(!base) return base;
+      base.push(item);
+      return base;
+  }
+}, null);
+
+
+/**
+ * 主过程
+**/
+
+// 处理通配符
+watchingList = Promise.all(
+  watchingList.map(pattern => new Promise((resolve, reject) => {
+    glob(pattern, (error, list) => {
+      return error ? reject(error) : resolve(list);
+    });
+  }))
+).then(list => [].concat(...list))
+
+// watch 每一个文件
+.then(list => Promise.all(list.map(file => {
+  bfs.stat(file).then(state => {
+    if(!state.isFile()) return;
+    return getHash(file);
+  }).then(hash => {
+    // 如果不是文件则什么也不做
+    if(!hash) return;
+    bfs.watchFile(file, { interval: 500 }, () => {
+      getHash(file).then(newHash => {
+        // 只有内容变化的时候才会触发
+        if(hash === newHash) return;
+        var child = childProcess.exec(commandList.join('\n'), {
+          env: { src: file }
+        });
+        child.stdout.pipe(process.stdout);
+        child.stderr.pipe(process.stderr);
+        hash = newHash;
+      });
+    });
+  });
+})))
+
+// 错误处理
+.catch(error => {
+  console.error(error);
+});

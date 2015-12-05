5c1f1ee1456e6e56d5b92f6d490f10bb9e450db0
Carter Li <zhangsongcui3371@sina.com>
2015-12-05 23:33:07 +0800

    支持打包 CSS 文件

--- src/usemin.js
+++ src/usemin.js
@@ -5,6 +5,7 @@ import bfs from 'babel-fs';
 import http from 'http';
 import UglifyJS from 'uglify-js';
 import argollector from 'argollector';
+import CleanCss from 'clean-css';
 
 
 /**
@@ -51,7 +52,7 @@ Promise
     return list;
   })
 
-  // 将 staticList 的文件重命名为带 hash 的
+  // 打包 js 和 css 文件
   .then(list => {
     var tasks = [];
     return Promise.all(list.map(pathname => {
@@ -60,30 +61,55 @@ Promise
         data += '';
         data = data.replace(/<!--\s*build\s([\s\S]+?)-->([\s\S]*?)<!--\s*endbuild\s*-->/g, ($0, configs, content) => {
           configs = matchUsemin(configs);
+          if (!configs.file || !configs.href) {
+            throw new Error('Missing essential attributes for <!-- build --> blocks:\n' + $0);
+          }
+
           // 从 HTML 片段中搜索并读入引用的文件
           var list = [];
           var tagMatcher = /<(?:script|link)([\s\S]*?)>/ig;
           while(tagMatcher.exec(content)) {
             let item = matchUsemin(RegExp.$1);
             let loader =/^http:/.test(item.file) ? loadRemoteData(item.file) : bfs.readFile(item.file, 'utf8');
-            loader = loader.then(data => {
-              // 将 css 转换成 js，并和其他 JS 一起合并起来
-              if(/\.css$/.test(item.file)) {
-                data = `document.write(${JSON.stringify('<style>' + data + '</style>')});`;
+
+            if (configs.file.endsWith('.js')) {
+              loader = loader.then(data => {
+                // 将 css 转换成 js，并和其他 JS 一起合并起来
+                if(/\.css$/.test(item.file)) {
+                  data = `document.write(${JSON.stringify('<style>' + data + '</style>')});`;
+                } else if (!item.file.endsWith('.js')) {
+                  throw new Error('Not supported source file type: ' + item.file);
+                }
+                // 为末尾不是 \n 的文件补上，否则 join 的时候可能因为上一个文件尾的单行注释而出问题
+                return data.toString().replace(/[^\n]$/, '$&\n');
+              });
+            } else if (configs.file.endsWith('.css')) {
+              if (item.file.endsWith('.css')) {
+                // css 文件不加换行也是安全的
+                loader = loader.then(data => data.toString());
+              } else {
+                throw new Error('Not supported source file type: ' + item.file);
               }
-              // 为末尾不是 \n 的文件补上，否则 join 的时候可能因为上一个文件尾的单行注释而出问题
-              return data.toString().replace(/[^\n]$/, '$&\n');
-            });
+            } else {
+              throw new Error('Not supported target file type: ' + configs.file)
+            }
             list.push(loader);
           }
           // 保存文件
           var task = Promise.all(list).then(list => {
-            var result = UglifyJS.minify(list.join(''), { fromString: true });
-            bfs.writeFile(configs.file, result.code);
+            var result = list.join('');
+            if (configs.file.endsWith('.js')) {
+              result = UglifyJS.minify(result, { fromString: true }).code;
+            } else {
+              result = new CleanCss().minify(result).styles;
+            }
+            bfs.writeFile(configs.file, result);
           });
           // 保存任务并替换字符串
           tasks.push(task);
-          return `<script src="${configs.href}"></script>`;
+          return configs.file.endsWith('.js')
+            ? `<script src="${configs.href}"></script>`
+            : `<link href="${configs.href}" rel="stylesheet" />`;
         });
         return bfs.writeFile(pathname, data);
       });
@@ -93,7 +119,7 @@ Promise
   // 成功
   .then(data => {
     // TODO
-  }) 
+  })
 
   // 错误处理到 stderr
   .catch(error => {
---
+++ tests/src/test8-2.js
@@ -0,0 +1,3 @@
+onload = function() {
+  document.body.innerHTML = str;
+};

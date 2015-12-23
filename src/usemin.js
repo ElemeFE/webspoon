bbeebabcbb017acc4e34ca8f3c04eb82c8390690
YanagiEiichi <admin@web-tinker.com>
2015-12-23 20:02:29 +0800

    Extract matcher for usemin

--- src/usemin.js
+++ src/usemin.js
@@ -3,8 +3,9 @@
 import glob from 'glob';
 import bfs from 'babel-fs';
 import http from 'http';
-import UglifyJS from 'uglify-js';
 import argollector from 'argollector';
+
+import UglifyJS from 'uglify-js';
 import CleanCss from 'clean-css';
 
 
@@ -12,17 +13,20 @@ import CleanCss from 'clean-css';
  * 通用声明
 **/
 
+var blockMatcher = /<!--\s*build\s([\s\S]+?)-->([\s\S]*?)<!--\s*endbuild\s*-->/g;
+var tagMatcher = /<(?:script|link)([\s\S]*?)>/ig;
+
 var matchUsemin = string => {
   var file = /file\s*=\s*"(.*?)"|$/.exec(string)[1];
   var href = /(?:href|src)\s*=\s*"(.*?)"|$/.exec(string)[1];
-  if(file === void 0 && href) file = href.replace(/^\/(?!\/)/, '');
-  if(/^\/\//.test(file)) file = 'http:' + file;
+  if (file === void 0 && href) file = href.replace(/^\/(?!\/)/, '');
+  if (/^\/\//.test(file)) file = 'http:' + file;
   return { file, href };
 };
 
 var loadRemoteDataCache = {};
 var loadRemoteData = url => {
-  if(loadRemoteDataCache[url]) return;
+  if (loadRemoteDataCache[url]) return;
   return loadRemoteDataCache[url] = new Promise((resolve, reject) => {
     http.get(url, res => {
       var buffers = [];
@@ -61,23 +65,20 @@ Promise
       // 此处不使用 Promise 扁平化是因为文件数据量可能很大，这样可以避免全部文件一起读入内存使内存占用过高
       return bfs.readFile(pathname).then(data => {
         data += '';
-        data = data.replace(/<!--\s*build\s([\s\S]+?)-->([\s\S]*?)<!--\s*endbuild\s*-->/g, ($0, configs, content) => {
+        data = data.replace(blockMatcher, ($0, configs, content) => {
           configs = matchUsemin(configs);
           if (!configs.file || !configs.href) {
             throw new Error('Missing essential attributes for <!-- build --> blocks:\n' + $0);
           }
-
           // 从 HTML 片段中搜索并读入引用的文件
           var list = [];
-          var tagMatcher = /<(?:script|link)([\s\S]*?)>/ig;
-          while(tagMatcher.exec(content)) {
+          while (tagMatcher.exec(content)) {
             let item = matchUsemin(RegExp.$1);
             let loader =/^http:/.test(item.file) ? loadRemoteData(item.file) : bfs.readFile(item.file, 'utf8');
-
             if (configs.file.match(/\.js$/)) {
               loader = loader.then(data => {
                 // 将 css 转换成 js，并和其他 JS 一起合并起来
-                if(/\.css$/.test(item.file)) {
+                if (/\.css$/.test(item.file)) {
                   data = `document.write(${JSON.stringify('<style>' + data + '</style>')});`;
                 } else if (!item.file.match(/\.js$/)) {
                   throw new Error('Not supported source file type: ' + item.file);
@@ -97,17 +98,21 @@ Promise
           }
           // 保存文件
           var task = Promise.all(list).then(list => {
-            var result = configs.file.match(/\.js$/)
-              ? UglifyJS.minify(list.join('\n;'), { fromString: true }).code
-              : new CleanCss().minify(list.join('\n')).styles;
-
+            var result;
+            if (/\.js$/.test(configs.file)) {
+              result = UglifyJS.minify(list.join('\n;'), { fromString: true }).code;
+            } else {
+              result = new CleanCss().minify(list.join('\n')).styles;
+            }
             bfs.writeFile(configs.file, result);
           });
           // 保存任务并替换字符串
           tasks.push(task);
-          return configs.file.match(/\.js$/)
-            ? `<script src="${configs.href}"></script>`
-            : `<link href="${configs.href}" rel="stylesheet" />`;
+          if (/\.js$/.test(configs.file)) {
+            return `<script src="${configs.href}"></script>`;
+          } else {
+            return `<link href="${configs.href}" rel="stylesheet" />`;
+          }
         });
         return bfs.writeFile(pathname, data);
       });
